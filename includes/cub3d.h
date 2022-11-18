838e154fbbd01a456d73adb41ebd82626f72e249
Arthur Faby <afaby@3A6.42angouleme.fr>
2022-11-18 09:29:27 +0100

    add struct to .h

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -2,5 +2,33 @@
 # define CUB3D_H
 
 # include <stdio.h>
+# include "libft.h"
+# include <fcntl.h>
+
+
+typedef struct s_player
+{
+	double	x;
+	double	y;
+	double	angle;
+}	t_player;
+
+typedef struct s_map
+{
+	int			**map;
+	int			width;
+	int			height;
+	t_player	player;
+}	t_map;
+
+typedef struct s_texture
+{
+	void	*north;
+	void	*south;
+	void	*west;
+	void	*east;
+	int		floor;
+	int		ceiling;
+}	t_texture;
 
 #endif
